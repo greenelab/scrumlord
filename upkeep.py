a95c58fde18fee5de7e515698505be5f080c60ea
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-04-20 17:15:40 -0400

    Add script to close old scrum issues

--- upkeep.py
+++ upkeep.py
@@ -1,7 +1,9 @@
+import argparse
 import datetime
 import re
 
 import holidays
+import github
 
 penn_holidays = {
     'Independence Day',
@@ -47,3 +49,37 @@ def issue_title_to_date(title: str):
     if not match:
         return None
     return datetime.date(*map(int, match.groups()))
+
+
+def close_old_issues(repo, lifespan: int):
+    """
+    Close scrum issues older than the number of days specified by lifespan.
+    """
+    lifespan = datetime.timedelta(lifespan)
+    today = datetime.date.today()
+    for issue in repo.get_issues():
+        title = issue.title
+        date = issue_title_to_date(title)
+        if not date:
+            continue
+        if today - date > lifespan:
+            print('Closing', title)
+            issue.edit(state='closed')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--username', default='scrum-lord')
+    parser.add_argument('--token')
+    parser.add_argument('--lifespan', type=int, default=7)
+    args = parser.parse_args()
+
+    gh = github.Github(args.username, args.token)
+    user = gh.get_user()
+
+    # Get greenelab/scrum repository. Could not find a better way
+    for repo in user.get_repos():
+        if repo.full_name == 'greenelab/scrum':
+            break
+
+    close_old_issues(repo, args.lifespan)
